e0e3d4e2af03ed6f104567d6c29f84c659f03d1f
Deepak <deepakviji5556@gmail.com>
2025-02-16 11:37:52 +0530

    Vip Card

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,5 +1,8 @@
 import Navbar from "@/components/page-components/Navbar";
 import PassCard from "@/components/page-components/PassCard";
+import ProCard from "@/components/page-components/ProCard";
+import ProPassCard from "@/components/page-components/ProPassCard";
+import ProPassList from "@/components/page-components/ProPassList";
 import FooterSection from "@/components/sections/footer/FooterSection";
 import React from "react";
 
@@ -7,7 +10,30 @@ function HomePage() {
   return (
     <div>
       <Navbar />
-      <PassCard />
+
+
+    
+
+      <ProPassList/>
+      {/* <ProPassList/> */}
+
+
+
+      {/* <PassCard /> */}
+      {/* <div className="flex justify-center items-center min-h-screen bg-gray-800">
+        <ProCard
+          title="Pro Plus Pass"
+          price={500}
+          dates="15-16/03/2024"
+          features={[
+            "Full Event Access (Day 1 + Day 2)",
+            "Exclusive Meet & Greet with Resource Persons",
+            "Priority Seating",
+            "Backstage Access",
+            "VIP Lounge Entry",
+          ]}
+        />
+      </div> */}
       <FooterSection />
     </div>
   );
