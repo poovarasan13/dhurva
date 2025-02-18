25c95b3e8a08f08073aebaab37a014aab8ed11bf
Siranjeevi <siranjeevi0619@gmail.com>
2025-02-18 13:43:35 +0530

    fixed components

--- src/components/page-components/AnimatedCard.jsx
+++ src/components/page-components/AnimatedCard.jsx
@@ -5,12 +5,16 @@ import '../../assets/fonts/dmsans.css';
 
 const AnimatedCard = () => {
   return (
-    <div className="flex bg-gray-100 justify-center items-center mt-16">
-      <div className="p-4">
+    <div className="flex bg-gray-200 justify-center items-center mt-16">
+      <div className="my-20">
         <div className="card">
           <div className="bg">
             <div className="p-3">
-                <h3 className="text-center text-3xl dm-sans  text-orange-400  ">Pro Pass</h3></div>{" "}
+                <h3 className="text-center text-3xl dm-sans  text-black  ">Pro Pass</h3></div>{" "}
+                <p className="text-2xl text-center">Rs : 200</p>
+                <ul>
+                  <li></li>
+                </ul>
           </div>
           <div className="blob"></div>
         </div>
