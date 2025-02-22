3d828141f182a5718b9d51992cb8d07c7f5e8ebf
Deepak <deepakviji5556@gmail.com>
2025-02-22 10:29:45 +0530

    announcement Speeling Changed

--- src/pages/Registration/RegistrationPage.jsx
+++ src/pages/Registration/RegistrationPage.jsx
@@ -6,7 +6,7 @@ function RegistrationPage() {
       <div className="h-max my-80 text-center">
         <h4 className="text-4xl dm-sans">
           {" "}
-          <span className="text-orange-300">Annoucement</span>{" "}
+          <span className="text-orange-300">Announcement</span>{" "}
           <span className="text-blue-300">Soon</span>
         </h4>
       </div>
