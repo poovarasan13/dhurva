d5d50cd2501b47c35c5521437e846f06ae2697a9
Siranjeevi <siranjeevi0619@gmail.com>
2025-02-24 18:13:45 +0530

    fixed registration page

--- src/pages/Registration/RegistrationPage.jsx
+++ src/pages/Registration/RegistrationPage.jsx
@@ -3,8 +3,8 @@ import "../../assets/fonts/dmsans.css";
 function RegistrationPage() {
   return (
     <div>
-      <div className="h-max my-80  text-center">
-        <h4 className="text-7xl tracking-widest powergrok">
+      <div className="h-max md:my-64 lg:my-80 my-60   text-center">
+        <h4 className="lg:text-7xl md:text-5xl text-lg  tracking-widest powergrok">
           {" "}
           <span className="text-orange-300">Announcement</span>{" "}
           <span className="text-blue-300">Soon</span>
